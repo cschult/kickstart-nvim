e82ae01dd1f5c219c9931e41a7b8393fd01c5291
cs <cs@jazz [core]>
2024-05-10 06:59:05 +0200

    set termguicolors

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,6 +25,9 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+-- use 24-bit RGB colors
+vim.opt.termguicolors = true
+
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
