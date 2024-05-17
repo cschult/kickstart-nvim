d0a9c1154f17d096d37568870a67ce56d896f113
cs <cs@jazz>
2024-05-17 16:17:01 +0200

    make lsp diagnostic ignore "missing fields"

--- .config/nvim/lua/kickstart/plugins/debug.lua
+++ .config/nvim/lua/kickstart/plugins/debug.lua
@@ -28,6 +28,7 @@ return {
     local dap = require 'dap'
     local dapui = require 'dapui'
 
+    ---@diagnostic disable: missing-fields
     require('mason-nvim-dap').setup {
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
